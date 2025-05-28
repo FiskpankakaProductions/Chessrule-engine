94f2d981007b9b00008e59c0a200adc272c51520
SwedishPotat0 <swedishpotato80@gmail.com>
2025-05-28 10:00:05 +0200

    Dosent work :(

--- main/main.cpp
+++ main/main.cpp
@@ -2,7 +2,9 @@
 using namespace std;
 
 int main() {
-  cout << "Hello World!";
+  int x;
+  cout << "Type a number: "; // Type a number and press enter
+  cin >> x; // Get user input from the keyboard
+  cout << "Your number is: " << x;
   return 0;
 }
-
