d35efa6b3d13f66b55a273522fdc0a7d73b418ac
SwedishPotat0 <swedishpotato80@gmail.com>
2025-05-29 14:46:14 +0200

    Update main.cpp

--- main/main.cpp
+++ main/main.cpp
@@ -3,37 +3,31 @@ using namespace std;
 
 int action;
 
+bool running = true;
+
 void start() {
 
 }
 
 void settings(){
-  cout << "[1]: GUI\n[2]: Logic\n[0]: Exit";
-  cin >> action;
-  if (action == 1) {
-
-  } else if (action == 2) {
-
-  } else if (action == 0) {
-
-  } else {
-    cout << "Unkown Command";
-  }
+  
 }
 
 int main() {
-  cout << "Menu\n";
-  cout << "[1]: Start\n[2]: Settings\n[0]: Exit\n";
-  cin >> action;
-  if (action == 1) {
-    start();
-  } else if (action == 2) {
-    settings();
-  } else if (action == 0) {
-    
-  } else {
-    cout << "Unkown Command";
+  while (running)
+  {
+    cout << "Menu\n";
+    cout << "[1]: Start\n[2]: Settings\n[0]: Exit\n";
+    cin >> action;
+    if (action == 1) {
+      start();
+    } else if (action == 2) {
+      settings();
+    } else if (action == 0) {
+      running = false;
+    } else {
+      cout << "Unkown Command";
+    }
   }
-
   return 0;
 }
