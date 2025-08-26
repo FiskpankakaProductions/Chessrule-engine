324036fc2a23b796ec53f230a0b909efa267d3fd
william82p <63519928+william82p@users.noreply.github.com>
2025-08-26 22:43:29 +0200

    Update main.cpp
    -Separated functions
    -Added basic piece movment aswell as UI
    
    Ask me if anything is unclear

--- main/main.cpp
+++ main/main.cpp
@@ -1,31 +1,46 @@
 #include <iostream>
 #include <bitset>
 
-void clearScreen() {
-    std::cout << "\033[2J\033[1;1H";
+
+std::bitset<4> board[8][8] = {
+    {8, 4, 6, 10, 12, 6, 4, 8},
+    {2, 2, 2, 2, 2, 2, 2, 2},
+    {0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0},
+    {3, 3, 3, 3, 3, 3, 3, 3},
+    {9, 5, 7, 11, 13, 7, 5, 9}
+};
+
+void printBoard() {
+    for (int row = 7; row >= 0; row--) {
+        for (int col = 0; col < 8; col++) {
+            std::cout <<  board[row][col] << " ";
+        }
+        std::cout << "\n";
+    }
 }
 
-int main(){
+void makeMove(const std::string& move) {
+    int x1 = move[0] - '0' - 1;
+    int y1 = move[1] - '0' - 1;
+    int x2 = move[2] - '0' - 1;
+    int y2 = move[3] - '0' - 1;
+    board[y2][x2] = board[y1][x1];
+    board[y1][x1] = 0;
+}
 
-	clearScreen();
-
-	std::bitset<4> board[8][8] =  {
-		{8, 4, 6, 10, 12, 6, 4, 8},
-		{2, 2, 2, 2, 2, 2, 2, 2},
-		{0, 0, 0, 0, 0, 0, 0, 0},
-		{0, 0, 0, 0, 0, 0, 0, 0},
-		{0, 0, 0, 0, 0, 0, 0, 0},
-		{0, 0, 0, 0, 0, 0, 0, 0},
-		{3, 3, 3, 3, 3, 3, 3, 3},
-		{9, 5, 7, 11, 13, 7, 5, 9}
-	};
-
-	for (int row = 0; row < 8; row++) {
-        	for (int col = 0; col < 8; col++) {
-            		std::cout << board[row][col] << " ";
-        	}
-        	std::cout << "\n";
-    	}
-
-	return 0;
+int main(int argc, char const *argv[]) {
+    printBoard();
+    while (true) {
+        std::string move;
+        std::cin >> move;
+        if (move.length() == 4) {
+            makeMove(move);
+            printBoard();
+        }
+    }
+    
+    return 0;
 }
