6d1ea5a4f107cc3ed9141b418e61a13689d51f27
SwedishPotat0 <swedishpotato80@gmail.com>
2025-08-27 11:53:14 +0200

    fixed movng function

--- main/main.cpp
+++ main/main.cpp
@@ -17,11 +17,12 @@ std::bitset<4> board[8][8] = {
 };
 
 void printBoard() {
+	std::cout << " 1  | 2  | 3  | 4  | 5  | 6  | 7  | 8  \n";
     for (int row = 7; row >= 0; row--) {
         for (int col = 0; col < 8; col++) {
             std::cout <<  board[row][col] << " ";
         }
-        std::cout << "\n";
+        std::cout << row+1  << "\n";
     }
 }
 
@@ -30,8 +31,10 @@ void makeMove(const std::string& move) {
     int y1 = move[1] - '0' - 1;
     int x2 = move[2] - '0' - 1;
     int y2 = move[3] - '0' - 1;
-    board[y2][x2] = board[y1][x1];
-    board[y1][x1] = 0;
+    if (board[y1][x1].to_ulong() > 0) {
+    	board[y2][x2] = board[y1][x1];
+    	board[y1][x1] = 0;    
+	}
 }
 
 int main() {
@@ -47,7 +50,7 @@ int main() {
 	}
 	else if (move == "q") {
 		break;
-        }
+        }	
 	else {
 		clearScreen();
 		printBoard();
