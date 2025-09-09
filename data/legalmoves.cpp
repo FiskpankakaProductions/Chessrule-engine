e0313ebe77278b8eeab53939d0da6bc53d9f76cd
SwedishPotat0 <swedishpotato80@gmail.com>
2025-09-09 17:35:25 +0200

    Have begun on legalmoves

--- data/legalmoves.cpp
+++ data/legalmoves.cpp
@@ -1,4 +1,47 @@
 #include "data.h"
+
 #include <iostream>
+#include <string>
 
+bool isLegal (int pice, std:string move) {
+	if (pice % 2 == 0) {
+		if (pice == 2) {
 
+		}
+		else if (pice == 4) {
+
+		}
+		else if (pice == 6) {
+
+		}
+		else if (pice == 8) {
+
+		}
+		else if (pice == 10) {
+
+		}
+		else if (pice == 12) {
+
+		}
+	}
+	else {
+		if (pice == 3) {
+
+		}
+		else if (pice == 5) {
+
+		}
+		else if (pice == 7) {
+
+		}
+		else if (pice == 9) {
+
+		}
+		else if (pice == 11) {
+
+		}
+		else if (pice == 13) {
+
+		}
+	}	
+}
