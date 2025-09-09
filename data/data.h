db16dd302a9e1865e932d140f203a6791b5a3acb
SwedishPotat0 <swedishpotato80@gmail.com>
2025-09-09 12:47:54 +0200

    Setup for multifile code

---
+++ data/data.h
@@ -0,0 +1,6 @@
+#ifndef data_H
+#define data_H
+
+// put all functions in .cpp files in /data here
+
+#endif
--- main/main.cpp
+++ main/main.cpp
@@ -4,6 +4,8 @@
 #include <vector>
 #include <cstdint>
 
+#include "../data/data.h"
+
 void clearScreen() {
     std::cout << "\033[2J\033[1;1H";
 }
@@ -120,6 +122,7 @@ int main() {
         turn = !turn;
         clearScreen();
         printBoard();
+	lastMove(move);
     }
     
     return 0;
